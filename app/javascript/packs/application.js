db76bf76258a4643eecba1b0d16f34811fc0bc67
wickedlasere <ziporrar@gmail.com>
2024-07-06 10:29:20 +0900

    application.js追加

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,7 @@
+import "../stylesheets/application.scss";
+import Rails from "@rails/ujs";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+
+Rails.start();
+ActiveStorage.start();

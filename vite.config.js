aea8901c1249aa8a07639c1c99229abcacb4c52b
wickedlasere <ziporrar@gmail.com>
2024-07-06 10:29:20 +0900

    application.js追加

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
+import RubyPlugin from 'vite-plugin-ruby';
 
 export default defineConfig({
-    root: '.',
-    build: {
-      rollupOptions: {
-        input: '/index.html',
-      },
+  plugins: [
+    RubyPlugin(),
+  ],
+  build: {
+    rollupOptions: {
+      input: './app/javascript/packs/application.js',
     },
-  });
+  },
+});
